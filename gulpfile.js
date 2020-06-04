3e4ce67df47a5fa450419c470506374ff13e0b50
Asiri Harisandu <aharisandu@cloudentity.com>
2020-06-04 13:01:36 +0530

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('./assets/scss/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./assets/css'));
+});
+
+
